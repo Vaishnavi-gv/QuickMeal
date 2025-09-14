56d55263b11272603476066c4f9a4f4c8a7c77e7
Vaishnavi-gv <156957524+Vaishnavi-gv@users.noreply.github.com>
2025-09-14 19:24:25 +0530

    database connected

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectdb= async() =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("db connected")
+    }
+    catch{
+        console.log("db error")
+    }
+}
+
+export default connectdb
